78a46fdb898e1fe329b03000f277b301853b936d
Drudge rajen <drudge.rajan@gmail.com>
2017-11-26 13:24:10 +0545

    map loaded

--- public/js/map.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function () {
-    initMap();
-});
-function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
-        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-    });
-}
--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -1,14 +1,36 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Simple Map</title>
-    <meta name="viewport" content="initial-scale=1.0">
-    <meta charset="utf-8">
-  <link rel="stylesheet" href="{{asset('css/style.css')}}">
-    <script type="javascript" href="{{asset('js/map.js')}}"></script>
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCK6sWy2ZLqhjSPPyrb7W9eYEwj8SFs5tE">
+    </script>
+
+    <script type="text/javascript" href="{{asset('js/map.js')}}"></script>
 </head>
 <body>
-@yield('content')
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCK6sWy2ZLqhjSPPyrb7W9eYEwj8SFs5tE"></script>
+<h3>Tweets on Map</h3>
+<div id="map"></div>
+
+<script>
+    function initMap(){
+        var myLatLng = {
+            lat: 43.6222102,
+            lng: -79.6694881
+        };
+
+        var map = new google.maps.Map(document.getElementById('map'), {
+            zoom: 15,
+            center: myLatLng
+        });
+
+        var marker = new google.maps.Marker({
+            position: myLatLng,
+            map: map,
+        });
+    }
+    google.maps.event.addDomListener(window, 'load', initMap);
+
+</script>
+
 </body>
 </html>
