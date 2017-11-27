02121161300ceb56122f29c79ff54e0439c0ccd3
Drudge rajen <drudge.rajan@gmail.com>
2017-11-27 21:31:21 +0545

    code refactored

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,20 +2,20 @@
 <html>
 <head>
     <title> Tweets on Map </title>
-    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/home/css/style.css')}}">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
         <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCK6sWy2ZLqhjSPPyrb7W9eYEwj8SFs5tE&libraries=places">
     </script>
-    {{--<script type="text/javascript"--}}
-            {{--src="https://maps.googleapis.com/maps/api/js?v=3&libraries=places">--}}
-    {{--</script>--}}
 
-    <script type="text/javascript" href="{{asset('js/map.js')}}"></script>
 </head>
 <body>
 @yield('content')
 
 </body>
 <script type="text/javascript" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script>
+    var APP_URL = {!! json_encode(url('/')) !!}
+</script>
+<script type="text/javascript" src="{{asset('assets/home/js/map.js')}}"></script>
 </html>
