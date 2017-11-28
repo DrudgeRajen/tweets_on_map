b224bb9a3a193216503e78f49eb7e0ef81c85465
Drudge rajen <drudge.rajan@gmail.com>
2017-11-28 20:33:23 +0545

    fixing responsiveness

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <title> Map Based Search Application </title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
     <link rel="stylesheet" href="{{asset('assets/home/css/style.css')}}">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
